2d056b9374b1f5b012574020d7cd008b4901e76b
tpmanc <tpxtrime@mail.ru>
2014-05-13 19:18:05 +0300

    datchi_hola changed2

--- datchik_holla/datchik_holla.ino
+++ datchik_holla/datchik_holla.ino
@@ -5,9 +5,14 @@ void setup () {
   digitalWrite(2,HIGH);
   digitalWrite(3,LOW);
   Serial.begin(9600);
+  
 }
+int count = 0;
 void loop() {
   int val = analogRead(A0);
-  Serial.println(val);
-  delay(700); 
+  if (( val < 15 )&&( val > 11 )){
+    count = count + 1;
+    Serial.println(count);
+  }
+  delay(100); 
 }
