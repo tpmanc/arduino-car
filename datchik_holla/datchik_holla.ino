dbc29c1606dd4846ba0fc7d4283d04ded3d563e9
tpmanc <tpxtrime@mail.ru>
2014-05-13 19:51:39 +0300

    datchi_hola (che to rabotaet)

--- datchik_holla/datchik_holla.ino
+++ datchik_holla/datchik_holla.ino
@@ -7,12 +7,27 @@ void setup () {
   Serial.begin(9600);
   
 }
-int count = 0;
+int prevTime = 0;
+int curTime = 0;
+int time = 0;
+float carSpeed = 0;
+boolean wheelRotation = false;
 void loop() {
-  int val = analogRead(A0);
-  if (( val < 15 )&&( val > 11 )){
-    count = count + 1;
-    Serial.println(count);
+  if ( analogRead(A0)<14 ) {
+    //if (prevTime != 0) {
+      if (wheelRotation == false){
+        curTime= millis();
+        time = curTime - prevTime;
+        wheelRotation = true;
+        prevTime = curTime;
+        
+        carSpeed = 6.28*1*time*1000*60 ;
+        Serial.println(time);
+      }
+    //} 
   }
-  delay(100); 
+  if ( analogRead(A0)>60 ) {
+    wheelRotation = false; 
+  }
+  delay(30);
 }
