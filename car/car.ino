5311ed8125c1209dcbe4af358e0b5441a10e86be
tpmanc <tpxtrime@mail.ru>
2014-12-10 19:07:05 +0400

    Update car.ino

--- car/car.ino
+++ car/car.ino
@@ -1,63 +1,94 @@
-#include <SoftwareSerial.h>
-
+#include
 SoftwareSerial mySerial(10, 11); // RX, TX
-
-void setup() 
+int prevTime, curTime, time, speedIndex;
+double carSpeed, tempSpeed, speedLimit, wheelRadius;
+boolean wheelRotation;
+char str;
+void setup()
 {
-// set the data rate for the SoftwareSerial port
 mySerial.begin(57600);
-pinMode(13,OUTPUT);
-pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
-pinMode(6,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(9,OUTPUT);
-//pinMode(10,OUTPUT);
-//pinMode(A0,INPUT);
+pinMode(13,OUTPUT);// Лампочка
+pinMode(4,OUTPUT); // ЗАДНИЙ МОТОР
+pinMode(5,OUTPUT); // ЗАДНИЙ МОТОР
+pinMode(6,OUTPUT); // ПЕРЕНИЙ МОТОР
+pinMode(7,OUTPUT); // ПЕРЕДНИЙ МОТОР
+pinMode(2,OUTPUT); // ДАТЧИК
+pinMode(3,OUTPUT); // ДАТЧИК
+pinMode(A0,INPUT); // ВЫХОД С ДАТЧИКА
+digitalWrite(2,HIGH); // ПИТАНИЕ ДАТЧИКА
+digitalWrite(3,LOW); // ПИТАНИЕ ДАТЧИКА
+Serial.begin(9600);
+prevTime = 0;
+wheelRadius = 0.01; // !!!!РАДИУС КОЛЕСА!!!!В МЕТРАХ
+speedIndex = 220;
+int tempLimit=255;
 }
-
 void loop() // run over and over
 {
 if (mySerial.available()) {
-
-char str = mySerial.read();
+if (mySerial.overflow()){};
+str = mySerial.read();
 char str1 = mySerial.read();
 char str2 = mySerial.read();
 char str3 = mySerial.read();
-
+char speedMagor = mySerial.read();
+char speedMiddle = mySerial.read();
+char speedMinor = mySerial.read();
+if ( ((int(speedMagor)-48) >= 0) && ( (int(speedMiddle)-48) >= 0 ) && ( (int(speedMinor)-48) >= 0 ) ){
+speedLimit = (int(speedMagor)-48)*100+(int(speedMiddle)-48)*10+(int(speedMinor)-48);
+}
 if (str == 'F') {
-digitalWrite(13,HIGH);
-digitalWrite(4,HIGH);
-digitalWrite(5,LOW); 
+analogWrite(5,speedIndex);
+analogWrite(4,0);
 } else {
 if (str1 == 'B') {
-digitalWrite(13,LOW);
-digitalWrite(4,LOW);
-digitalWrite(5,HIGH); 
+analogWrite(5,0);
+analogWrite(4,255);
 } else {
-digitalWrite(5,LOW);
-digitalWrite(4,LOW); 
+analogWrite(5,0);
+analogWrite(4,0);
 }
 }
-
 if (str2 == 'L') {
-digitalWrite(13,HIGH);
-digitalWrite(6,HIGH);
-digitalWrite(7,LOW); 
+digitalWrite(7,HIGH);
+digitalWrite(6,LOW);
 }else {
 if (str3 == 'R') {
-digitalWrite(13,LOW);
-digitalWrite(6,LOW);
-digitalWrite(7,HIGH); 
+digitalWrite(7,LOW);
+digitalWrite(6,HIGH);
 }else {
 digitalWrite(6,LOW);
-digitalWrite(7,LOW); 
+digitalWrite(7,LOW);
 }
 }
-//millis() - подсчет времени в милисекундах 
-mySerial.print(str);
-mySerial.print(str1);
-mySerial.print(str2);
-mySerial.print(str3);
+}
+if ( analogRead(A0)<16 ) {
+if (wheelRotation == false){
+curTime= millis();
+time = curTime - prevTime;
+wheelRotation = true;
+prevTime = curTime;
+tempSpeed = 6.28*wheelRadius/time*100*60*60;//km/ч *10^(-2)
+if ((tempSpeed > 2)&&(tempSpeed < 200 )){
+Serial.println(carSpeed);
+carSpeed = tempSpeed;
+}
+if (carSpeed > speedLimit) {
+//speedIndex = (1 - speedLimit/carSpeed)*carSpeed - speedLimit);
+speedIndex = speedIndex * speedLimit / carSpeed + 90;
+if (speedIndex>220) {
+speedIndex = 220;
+}
+if (speedIndex<150) {
+speedIndex = 150;
+}
+if (speedLimit == 0){
+speedIndex = 220;
+}
+}
+}
+}
+if ( analogRead(A0)>100 ) {
+wheelRotation = false;
 }
 }
